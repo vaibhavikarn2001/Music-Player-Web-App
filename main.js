37f3b9476ada675b9fca61103d59d32b499fa67c
Vaibhavi Karn <76275888+vaibhavikarn2001@users.noreply.github.com>
2021-05-27 18:45:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,289 @@
+var track = document.getElementById('track');
+
+var controlBtn = document.getElementById('play-pause');
+
+function playPause() {
+    if (track.paused) {
+        track.play();
+        //controlBtn.textContent = "Pause";
+        controlBtn.className = "pause";
+    } else { 
+        track.pause();
+         //controlBtn.textContent = "Play";
+        controlBtn.className = "play";
+    }
+}
+
+controlBtn.addEventListener("click", playPause);
+track.addEventListener("ended", function() {
+  controlBtn.className = "play";
+});
+var track2 = document.getElementById('track2');
+
+var controlBtn2 = document.getElementById('play-pause2');
+
+function playPause2() {
+    if (track2.paused) {
+        track2.play();
+        //controlBtn.textContent = "Pause";
+        controlBtn2.className = "pause2";
+    } else { 
+        track2.pause();
+         //controlBtn.textContent = "Play";
+        controlBtn2.className = "play2";
+    }
+}
+
+controlBtn2.addEventListener("click", playPause2);
+track2.addEventListener("ended", function() {
+  controlBtn2.className = "play2";
+});
+var track3 = document.getElementById('track3');
+
+var controlBtn3 = document.getElementById('play-pause3');
+
+function playPause3() {
+    if (track3.paused) {
+        track3.play();
+        //controlBtn.textContent = "Pause";
+        controlBtn3.className = "pause3";
+    } else { 
+        track3.pause();
+         //controlBtn.textContent = "Play";
+        controlBtn3.className = "play3";
+    }
+}
+
+controlBtn3.addEventListener("click", playPause3);
+track3.addEventListener("ended", function() {
+  controlBtn3.className = "play3";
+});
+
+var track4 = document.getElementById('track4');
+
+var controlBtn4 = document.getElementById('play-pause4');
+
+function playPause4() {
+    if (track4.paused) {
+        track4.play();
+        //controlBtn.textContent = "Pause";
+        controlBtn4.className = "pause4";
+    } else { 
+        track4.pause();
+         //controlBtn.textContent = "Play";
+        controlBtn4.className = "play4";
+    }
+}
+
+controlBtn4.addEventListener("click", playPause4);
+track4.addEventListener("ended", function() {
+  controlBtn4.className = "play4";
+});
+
+var track5 = document.getElementById('track5');
+
+var controlBtn5 = document.getElementById('play-pause5');
+
+function playPause5() {
+    if (track5.paused) {
+        track5.play();
+        //controlBtn.textContent = "Pause";
+        controlBtn5.className = "pause5";
+    } else { 
+        track5.pause();
+         //controlBtn.textContent = "Play";
+        controlBtn5.className = "play5";
+    }
+}
+
+controlBtn5.addEventListener("click", playPause5);
+track5.addEventListener("ended", function() {
+  controlBtn5.className = "play5";
+});
+
+var track6 = document.getElementById('track6');
+
+var controlBtn6 = document.getElementById('play-pause6');
+
+function playPause6() {
+    if (track6.paused) {
+        track6.play();
+        //controlBtn.textContent = "Pause";
+        controlBtn6.className = "pause6";
+    } else { 
+        track6.pause();
+         //controlBtn.textContent = "Play";
+        controlBtn6.className = "play6";
+    }
+}
+
+controlBtn6.addEventListener("click", playPause6);
+track6.addEventListener("ended", function() {
+  controlBtn6.className = "play6";
+});
+var track7 = document.getElementById('track7');
+
+var controlBtn7 = document.getElementById('play-pause7');
+
+function playPause7() {
+    if (track7.paused) {
+        track7.play();
+        //controlBtn.textContent = "Pause";
+        controlBtn7.className = "pause7";
+    } else { 
+        track7.pause();
+         //controlBtn.textContent = "Play";
+        controlBtn7.className = "play7";
+    }
+}
+
+controlBtn7.addEventListener("click", playPause7);
+track7.addEventListener("ended", function() {
+  controlBtn7.className = "play7";
+});
+var track8 = document.getElementById('track8');
+
+controlBtn8 = document.getElementById('play-pause8');
+
+function playPause8() {
+    if (track8.paused) {
+        track8.play();
+        //controlBtn.textContent = "Pause";
+        controlBtn8.className = "pause8";
+    } else { 
+        track8.pause();
+         //controlBtn.textContent = "Play";
+        controlBtn8.className = "play8";
+    }
+}
+
+controlBtn8.addEventListener("click", playPause8);
+track8.addEventListener("ended", function() {
+  controlBtn8.className = "play8";
+});
+var track9 = document.getElementById('track9');
+
+var controlBtn9 = document.getElementById('play-pause9');
+
+function playPause9() {
+    if (track9.paused) {
+        track9.play();
+        //controlBtn.textContent = "Pause";
+        controlBtn9.className = "pause9";
+    } else { 
+        track9.pause();
+         //controlBtn.textContent = "Play";
+        controlBtn9.className = "play9";
+    }
+}
+
+controlBtn9.addEventListener("click", playPause9);
+track9.addEventListener("ended", function() {
+  controlBtn9.className = "play9";
+});
+var track10 = document.getElementById('track10');
+
+var controlBtn10 = document.getElementById('play-pause10');
+
+function playPause10() {
+    if (track10.paused) {
+        track10.play();
+        //controlBtn.textContent = "Pause";
+        controlBtn10.className = "pause10";
+    } else { 
+        track10.pause();
+         //controlBtn.textContent = "Play";
+        controlBtn10.className = "play10";
+    }
+}
+
+controlBtn10.addEventListener("click", playPause10);
+track10.addEventListener("ended", function() {
+  controlBtn10.className = "play10";
+});
+var track11 = document.getElementById('track11');
+
+var controlBtn11 = document.getElementById('play-pause11');
+
+function playPause11() {
+    if (track11.paused) {
+        track11.play();
+        //controlBtn.textContent = "Pause";
+        controlBtn11.className = "pause11";
+    } else { 
+        track11.pause();
+         //controlBtn.textContent = "Play";
+        controlBtn11.className = "play11";
+    }
+}
+
+controlBtn11.addEventListener("click", playPause11);
+track11.addEventListener("ended", function() {
+  controlBtn11.className = "play11";
+});
+
+
+
+
+var track12 = document.getElementById('track12');
+
+var controlBtn12 = document.getElementById('play-pause12');
+
+function playPause12() {
+    if (track12.paused) {
+        track12.play();
+        //controlBtn.textContent = "Pause";
+        controlBtn12.className = "pause12";
+    } else { 
+        track12.pause();
+         //controlBtn.textContent = "Play";
+        controlBtn12.className = "play12";
+    }
+}
+
+controlBtn12.addEventListener("click", playPause12);
+track12.addEventListener("ended", function() {
+  controlBtn12.className = "play12";
+});
+
+
+vartrack13 = document.getElementById('track13');
+
+var controlBtn13 = document.getElementById('play-pause13');
+
+function playPause13() {
+    if (track13.paused) {
+        track13.play();
+        //controlBtn.textContent = "Pause";
+        controlBtn13.className = "pause13";
+    } else { 
+        track13.pause();
+         //controlBtn.textContent = "Play";
+        controlBtn13.className = "play13";
+    }
+}
+
+controlBtn13.addEventListener("click", playPause13);
+track13.addEventListener("ended", function() {
+  controlBtn13.className = "play13";
+});
+var track14 = document.getElementById('track14');
+
+var controlBtn14 = document.getElementById('play-pause14');
+
+function playPause14() {
+    if (track14.paused) {
+        track14.play();
+        //controlBtn.textContent = "Pause";
+        controlBtn14.className = "pause14";
+    } else { 
+        track14.pause();
+         //controlBtn.textContent = "Play";
+        controlBtn14.className = "play14";
+    }
+}
+
+controlBtn14.addEventListener("click", playPause14);
+track14.addEventListener("ended", function() {
+  controlBtn14.className = "play14";
+});
